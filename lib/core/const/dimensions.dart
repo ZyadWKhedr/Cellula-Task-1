de59602244e09cc6f3cb6aeed3ccf2b52c32737d
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-02-11 14:06:05 +0200

    font sizes dimentions

--- lib/core/const/dimensions.dart
+++ lib/core/const/dimensions.dart
@@ -13,4 +13,11 @@ class Dimensions {
   static double width10(BuildContext context) => screenWidth(context) / 80;
   static double width20(BuildContext context) => screenWidth(context) / 40;
   static double width50(BuildContext context) => screenWidth(context) / 16;
+
+  // Font Sizes - Based on screen width for better responsiveness
+  static double font12(BuildContext context) => screenWidth(context) / 34;
+  static double font14(BuildContext context) => screenWidth(context) / 30;
+  static double font16(BuildContext context) => screenWidth(context) / 26;
+  static double font18(BuildContext context) => screenWidth(context) / 22;
+  static double font20(BuildContext context) => screenWidth(context) / 20;
 }
